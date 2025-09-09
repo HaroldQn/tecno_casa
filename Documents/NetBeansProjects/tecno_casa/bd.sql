8fa38b809882c3e472938df24c5a2a621e0c7724
Harold Quispe <128565012+HaroldQn@users.noreply.github.com>
2025-09-08 23:23:31 -0500

    Agregando modelo de encriptamiento y schema de bd

--- Documents/NetBeansProjects/tecno_casa/bd.sql
+++ Documents/NetBeansProjects/tecno_casa/bd.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE tecno_casa;
+
+CREATE TABLE usuarios(
+	idusuario int primary key auto_increment,
+	email varchar(150),
+	clave varchar(60),
+    nombre varchar(50)
+)ENGINE=INNODB;
+
--- Documents/NetBeansProjects/tecno_casa/src/main/java/Servicios/Encriptamiento.java
+++ Documents/NetBeansProjects/tecno_casa/src/main/java/Servicios/Encriptamiento.java
@@ -4,10 +4,30 @@
  */
 package Servicios;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 /**
  *
  * @author efrai
  */
 public class Encriptamiento {
+    public static String encriptar(String textoPlano) {
+        return BCrypt.hashpw(textoPlano, BCrypt.gensalt());
+    }
+    public static boolean verificar(String textoPlano, String hash) {
+        return BCrypt.checkpw(textoPlano, hash);
+    }
     
+    public static void main(String[] args) {
+        String password = "hola123";
+
+        // Encriptar
+        String hash = encriptar(password);
+        System.out.println("Original: " + password);
+        System.out.println("Encriptada (hash): " + hash);
+
+        // Verificar
+        boolean coincide = verificar("hola123", hash);
+        System.out.println("¿Coincide? " + coincide);
+    }
 }
